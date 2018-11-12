5d45565b6fe47d4bf9a46180799efb7cda97c436
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-12 15:38:52 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,15 @@
 class Dog
+  attr_accessor :name, :age
+  attr_reader :breed
+
+  def all
+    @@all
+  end
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@all.push(self)
   end
 end
